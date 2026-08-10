6817281a4213ca292b24eec984bedc6bbeeb7ee2
Omid Hassibi <omid.d.hassibi@gmail.com>
2026-08-10 12:12:38 -0700

    Added Multiplication

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     public static double operation () {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Which operation would you like to complete today?");
-        System.out.println("Type A for Addition and S for subtraction:");
+        System.out.println("Type A for Addition, S for subtraction, M for multiplication:");
         String operator = scanner.nextLine();
 
         String path;
@@ -20,6 +20,9 @@ public class Calculator {
         } else if (operator.equals("S")) {
             System.out.println("Subtraction it is!");
             path = "Subtraction";
+        } else if (operator.equals("M")) {
+            System.out.println("Multiplication it is!");
+            path = "Multiplication";
         } else {
             System.out.println("Please try again.");
             path = "Unknown";
@@ -35,11 +38,14 @@ public class Calculator {
         if (path.equals("Addition")) {
             value = addition(a, b);
             return value;
-            
         } else if (path.equals("Subtraction")) {
             value = subtraction(a, b);
             return value;
-        } else {
+        } else if (path.equals("Multiplication")) {
+            value = multiplication(a, b);
+            return value;
+        } 
+        else {
             System.out.println("Please run the program again and choose a valid response");
             return 0.0;
         }
@@ -51,6 +57,9 @@ public class Calculator {
     }
     public static double subtraction (double a, double b) { 
         return a - b;
+    }
+    public static double multiplication (double a, double b) {
+        return a * b;
     }
 
 
