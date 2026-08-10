8eeab32e23f063ef9dc65a46dff32b1faa8ac50f
Omid Hassibi <omid.d.hassibi@gmail.com>
2026-08-10 09:55:29 -0700

    Addition and Subtraction added

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,66 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         // This is a calculator 
         System.out.println("Welcome!");
+        System.out.println(operation());
+
     }
+    public static double operation () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which operation would you like to complete today?");
+        System.out.println("Type A for Addition and S for subtraction:");
+        String operator = scanner.nextLine();
+
+        String path;
+        if (operator.equals("A")) {
+            System.out.println("Addition it is!");
+            path = "Addition";
+        } else if (operator.equals("S")) {
+            System.out.println("Subtraction it is!");
+            path = "Subtraction";
+        } else {
+            System.out.println("Please try again.");
+            path = "Unknown";
+            return 0.0;
+        }
+
+        System.out.println("Value of the first number:");
+        double a = Double.valueOf(scanner.nextLine());
+        System.out.println("Value of the second number:");
+        double b = Double.valueOf(scanner.nextLine());
+
+        double value;
+        if (path.equals("Addition")) {
+            value = addition(a, b);
+            return value;
+            
+        } else if (path.equals("Subtraction")) {
+            value = subtraction(a, b);
+            return value;
+        } else {
+            System.out.println("Please run the program again and choose a valid response");
+            return 0.0;
+        }
+        
+
+    }
+    public static double addition (double a, double b) {
+        return a + b;
+    }
+    public static double subtraction (double a, double b) { 
+        return a - b;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
